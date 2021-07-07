eac14d492613340973cb17c29d9e014a0ef590b8
azkanab <azkanabilah@gmail.com>
2021-07-07 20:13:44 +0700

    Finish handson for the second meeting

--- handson/handson.js
+++ handson/handson.js
@@ -8,9 +8,28 @@ const sumReducer = (acc, student) => {
     return acc + student.age;
 }
 
-const averageAge = (students) => {
+const calculateAverage = (students) => {
     let sum = students.reduce(sumReducer, 0);
     return sum / students.length;
+}
+
+const waitForSeconds = (time) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time)
+    })
+}
+
+const getAverageAge = async () => {
+    console.log('calculation start')
+    let result = calculateAverage(students)
+    await waitForSeconds(2000)
+    console.log('second call started')
+    await waitForSeconds(2000)
+    console.log('second call finished')
+    console.log('third call started')
+    await waitForSeconds(1000)
+    console.log(`age is ${result}`)
+    console.log('third call finished')
 }
 
 const studentDiv = document.querySelector('.students')
@@ -25,6 +44,5 @@ students.forEach((student) => {
 });
 
 function handleClick() {
-    let average = averageAge(students)
-    console.log(average)
+    getAverageAge()
 }
