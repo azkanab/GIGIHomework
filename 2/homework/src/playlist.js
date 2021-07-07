c3077322e95f3bad080770e3d19f2723f5227b9e
azkanab <azkanabilah@gmail.com>
2021-07-08 00:48:47 +0700

    Render track data from response - Week 1 Session 2

--- 2/homework/src/playlist.js
+++ 2/homework/src/playlist.js
@@ -1,7 +1,12 @@
 /* Initialization */
-const URL_GET_ALBUMS = 'https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json'
+const URL_GET_TRACK = 'https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json'
+const IMAGE_HEIGHT = 300 /* Choices: 640, 300, 64 */
+init()
 
-getAlbums()
+async function init() {
+	const trackData = await getTrack()
+	await renderPlaylist(trackData)
+}
 
 function handleSubmit() {
 	let title = document.querySelector('#title').value
@@ -13,12 +18,53 @@ function handleSubmit() {
 	}                                                                                                                                       
 }
 
-function getAlbums() {
-	axios.get(URL_GET_ALBUMS)
-		.then(response => {
-			console.log(response)
-		})
-		.catch(error => {
-			alert(`Error: ${error}`)
-		})
+function getTrack() {
+	function onFetchSuccessful(response) {
+		console.log(response.data)
+		return response.data
+	}
+
+	function onFetchError(error) {
+		alert(`Error: ${error}`)
+	}
+
+	return axios.get(URL_GET_TRACK)
+		.then(onFetchSuccessful)
+		.catch(onFetchError)
+}
+
+// getTrack async/await
+// async function anotherGetTrack() {
+// 	try {
+// 		const response = await axios.get(URL_GET_TRACK)
+// 		return response.data
+// 	} catch(e) {
+// 		alert(error)
+// 	}
+// }
+
+function findSmallImage(trackData) {
+	let images = trackData.album.images
+	let url = '../public/smiley.png'
+
+	images.forEach((image) => {
+		if (image.height === IMAGE_HEIGHT) {
+			url = image.url
+		}
+	})
+
+	return url
+}
+
+async function renderPlaylist(trackData) {
+	const playlistWrapper = document.querySelector('.wrapper')
+	let IMAGE_URL = findSmallImage(trackData)
+
+	playlistWrapper.innerHTML += `
+		<img src='${IMAGE_URL}' class="myImage">
+		<p>Track title: ${trackData.name}</p>
+		<p>Artist: ${trackData.artists[0].name}</p>
+		<p>Album: ${trackData.album.name}</p>
+		<button>Select</button>
+	`
 }
